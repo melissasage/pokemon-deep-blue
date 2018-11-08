0616b39647932f41286e5fda6bb4855aea7bdf37
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-11-08 12:34:43 -0500

    Added gen1 info from official Showdown repo

---
+++ info/index.js
@@ -0,0 +1,8 @@
+// gen1 is from the official pokemon showdown repo- it describes how battles work in gen1.
+// since gen1 is so different from later gens, it's a fairly decent description of the environment by itself.
+
+const typechart = require("./gen1/typechart");
+
+module.exports = {
+  typechart
+};
