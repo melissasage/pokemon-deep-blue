a319be08afc6acc121e4aecd8240ef03f321e9e4
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-11-08 15:01:33 -0500

    Configured ability to chat with Showdown server.

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const WebSocket = require("ws");
 const url = "ws://sim.smogon.com:8000/showdown/websocket";
 
-const webSocket = new WebSocket(url, {
+const ws = new WebSocket(url, {
   perMessageDeflate: false
   // headers: {
   //   Authorization: `Basic ${encodedToken}`,
   // },
 });
 
-webSocket.on("open", function open() {
+const nameMyself = name => `|\\updateuser ${name}`;
+const callYourMom = text => `|\\pm melissasage, ${text}`;
+
+ws.on("open", function open() {
   console.log("Connection has been established.");
+  ws.send(nameMyself("porygonbot1809"));
 });
 
-webSocket.on("close", function close() {
+ws.on("message", function incoming(message) {
+  console.log(`>> ${message}`);
+});
+
+ws.on("close", function close() {
   console.log("Connection closed.");
 });
+
+// console.log("Attempting to name myself...");
+// console.log("Name set!");
+// console.log("Sending a ping to Melissa on the server...");
+// ws.send(callYourMom("hi mom!"));
+// console.log("Said hi to mom.");
