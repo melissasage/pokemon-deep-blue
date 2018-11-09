ed104fc61ddf0c7769f84688845a8a45a5d965cb
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-11-09 10:55:05 -0500

    Feature: connect to server, login, and send messages.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Porygon
 
-_The very best, like no one ever was_
+_Pokemon Deep Blue Version_
 
 My contribution to the Stackathon of Grace Hopper cohort 1809 at Fullstack Academy.
 
 Porygon is an AI that plays Pokemon Generation I- Red, Blue, Yellow- through the Pokemon Showdown server.
+
+It is written in Javascript, with sections in Python.
--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 const WebSocket = require("ws");
+const axios = require("axios");
+const secrets = require("./secrets");
 const url = "ws://sim.smogon.com:8000/showdown/websocket";
 
-const ws = new WebSocket(url, {
-  perMessageDeflate: false
-  // headers: {
-  //   Authorization: `Basic ${encodedToken}`,
-  // },
-});
+const ws = new WebSocket(url);
+f
+const username = "porygon1809";
 
-const nameMyself = name => `|\\updateuser ${name}`;
-const callYourMom = text => `|\\pm melissasage, ${text}`;
+const callYourMom = text => `|/pm melissasage, ${text}`;
+// const nameMyself = name => `|updateuser `;
+const helloWorld = () => {
+  ws.send(callYourMom("hi mom!"));
+};
+
+const login = async challstr => {
+  try {
+    const res = await axios.post(`http://play.pokemonshowdown.com/action.php`, {
+      act: "login",
+      name: username,
+      pass: process.env.PASSWORD,
+      challstr
+    });
+    const assertion = JSON.parse(res.data.slice(1)).assertion;
+    return assertion;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 ws.on("open", function open() {
   console.log("Connection has been established.");
-  ws.send(nameMyself("porygonbot1809"));
+
+  ws.on("message", function incoming(message) {
+    console.log(`>> ${message}`);
+    if (getChallstr(message)) {
+      const assertion = await login(RegExp.$1);
+      ws.send(`|/trn ${username},0,${assertion}`);
+      helloWorld();
+    }
+  });
 });
 
-ws.on("message", function incoming(message) {
-  console.log(`>> ${message}`);
+ws.on("send", function incoming(message) {
+  console.log(`<< ${message}`);
 });
 
 ws.on("close", function close() {
   console.log("Connection closed.");
 });
-
-// console.log("Attempting to name myself...");
-// console.log("Name set!");
-// console.log("Sending a ping to Melissa on the server...");
-// ws.send(callYourMom("hi mom!"));
-// console.log("Said hi to mom.");
