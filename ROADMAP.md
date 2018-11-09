b010bfa6dec7e73fba941647a9169b88c7b0d6b8
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-11-09 16:28:14 -0500

    Feature: can select random moves and enact them in Showdown

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,5 +1,8 @@
 # Roadmap
 
-[] set up basic functions to interface with the Showdown API.
-[] set up a proof of concept AI that plays random moves.
+[x] set up basic functions to interface with the Showdown API.
+[x] set up a proof of concept AI that plays random moves.
+[] Fix bug preventing AI from switching when a pokemon faints.
+[] Fix bug preventing AI from logging in on its account.
+[] AI should re-challenge when a battle is concluded.
 [] implement an AI that learns through its plays, via TensorFlow.js.
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const WebSocket = require("ws");
 const axios = require("axios");
 const secrets = require("./secrets");
+const pickAction = require("./strategy/random");
 
 const url = "ws://sim.smogon.com:8000/showdown/websocket";
 const ws = new WebSocket(url);
 const username = "porygon1809";
 let loggedIn = false;
+let battleId = "";
+let status = "";
 const getChallstr = str => {
   const findChallStr = /^|challstr|([\S*])$/;
   return findChallStr.test(str);
@@ -32,16 +35,26 @@ const login = async challstr => {
 
 ws.on("open", function open() {
   console.log("Connection has been established.");
+});
 
-  ws.on("message", async function incoming(message) {
-    console.log(`>> ${message}`);
-    if (getChallstr(message) && loggedIn == false) {
-      loggedIn = true;
-      const assertion = await login(RegExp.$1);
-      ws.send(`|/trn ${username},0,${assertion}`);
-      startBattle();
+ws.on("message", async function incoming(message) {
+  console.log(`>> ${message}`);
+  if (getChallstr(message) && loggedIn == false) {
+    loggedIn = true;
+    const assertion = await login(RegExp.$1);
+    ws.send(`|/trn ${username},0,${assertion}`);
+    startBattle();
+  } else if (/(battle\-[\w\d]+\-[\d]+)/.test(message)) {
+    if (!battleId) battleId = RegExp.$1;
+    console.log("battleId:", battleId);
+    const request = /(\|request\|)(.*$)/.test(message);
+    if (request) {
+      status = RegExp.$2;
+    } else if (status && /\|turn\|\d/.test(message)) {
+      const action = pickAction(status, battleId);
+      ws.send(action);
     }
-  });
+  }
 });
 
 ws.on("send", function incoming(message) {
