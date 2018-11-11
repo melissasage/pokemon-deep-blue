f548ba641ea0cd989801461e5b4116fbecc3235e
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-11-11 17:49:50 -0500

    Final commit

---
+++ strategy/policy.js
@@ -0,0 +1,8 @@
+const tensorFlow = require("tensorflow");
+const tf = tensorFlow.tensor([1, 1]);
+
+const actions = ["attack", "switch"];
+console.log(tf);
+
+// tf.resetDefaultGraph();
+// const chosenAction = tf.max(weights)
